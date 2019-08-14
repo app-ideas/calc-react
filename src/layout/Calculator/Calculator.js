69e06d94a63866c270bff78cf956a7966a7551f5
root <root@Bimal>
2019-08-14 23:49:23 +0530

    Calculator UI

---
+++ src/layout/Calculator/Calculator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Screen from './Screen/Screen';
+import Keypad from './Keypad/Keypad';
+
+const calculator = () =>(
+<main className="calculator">
+    <Screen></Screen>
+    <Keypad></Keypad>
+</main>
+);
+
+export default calculator;
